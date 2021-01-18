afe05f7fdf4fb4bf528f9d2bda5aa01b148a27c7
tibraj <tibraj94@gmail.com>
2021-01-18 15:47:57 -0500

    some higher order

--- app.js
+++ app.js
@@ -10,6 +10,25 @@ function loop(start, test, update, body) {
 }
 
 loop(3, n => n > 0, n => n - 1, console.log);
+
+
 //Everything
+function every(array, predicate) {
+    for(let element of array) {
+        if(!predicate(element)) return false;
+    }
+    return true;
+}
+
+function secondEvery(array, predicate) {
+    return !array.some(element => !predicate(element));
+}
+
+console.log(every([1,3,5], n => n < 10));
+console.log(secondEvery([1,3,5], n => n < 10));
+console.log(every([2,4,16], n => n < 10));
+console.log(secondEvery([2,4,16], n => n < 10));
+console.log(every([], n => n < 10));
+console.log(secondEvery([], n => n < 10));
 
 //Dominant Writing Direction

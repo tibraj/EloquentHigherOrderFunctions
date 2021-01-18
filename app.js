4006cfde60d629d07c9eb0a48a38f9014dca0a0a
tibraj <tibraj94@gmail.com>
2021-01-18 15:44:48 -0500

    abstraction

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ let arrays = [[1,2,3], [4,5], [6]];
 console.log(arrays.reduce((flat, current) => flat.concat(current), []));
 
 //Your Own Loop
+function loop(start, test, update, body) {
+    for(let value = start; test(value); value = update(value)) {
+        body(value);
+    }
+}
 
+loop(3, n => n > 0, n => n - 1, console.log);
 //Everything
 
 //Dominant Writing Direction
